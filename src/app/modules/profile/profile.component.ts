5d5045e870fa399c7ccee0f34d6a2034f03caf90
JJLorente <joel.jimenez.lorente@gmail.com>
2020-04-21 23:22:03 +0200

    menu profile + boton cerrar sesion

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { AuthService } from 'src/app/core/auth/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,12 +9,18 @@ import { MenuController } from '@ionic/angular';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private menuCtrl: MenuController) { }
+  constructor(
+    private menuCtrl: MenuController,
+    private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   async onClickMenu(){
-    this.menuCtrl.toggle("profileMenu");
+    this.menuCtrl.toggle('profileMenu');
+  }
+
+  async logoutButton(){
+    this.authService.logOut();
   }
 }
